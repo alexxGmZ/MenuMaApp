e90d531037e7c7a070b066d73fefc840c5bb4faf
alexxShandsome <algilbertgomez@gmail.com>
2023-07-07 09:57:44 +0800

    setup tailwind css

--- Manage/tailwind.config.js
+++ Manage/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,js}",
+    "./main.js",
+    "./main.html"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
