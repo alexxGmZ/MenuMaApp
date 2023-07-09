c4bcedd02c07d88117fc09597bdcd90689cd5d5c
alexxShandsome <algilbertgomez@gmail.com>
2023-07-09 08:48:19 +0800

    1280x720 as minimum resolution

--- Manage/main.js
+++ Manage/main.js
@@ -5,8 +5,8 @@ const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
 	const win = new BrowserWindow({
-		width: 800,
-		height: 600
+		width: 1280,
+		height: 720
 	})
 
 	win.loadFile('./public/main.html')
