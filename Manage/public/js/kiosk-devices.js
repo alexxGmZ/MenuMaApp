6fbf4dc84273b32e0383f0302d8bc0c62640ffbc
alexxShandsome <algilbertgomez@gmail.com>
2023-08-17 12:51:30 +0800

    amateur network scanner

--- Manage/public/js/kiosk-devices.js
+++ Manage/public/js/kiosk-devices.js
@@ -1,10 +1,12 @@
 const mysql = require("mysql2");
+const ping = require("ping");
+const dns = require("dns");
 
 // create database connection
 var connection = mysql.createConnection({
 	host: "localhost",
-	user: "",
-	password: "",
+	user: "algilbert",
+	password: "Ilikefoods@123",
 	database: "manage_db"
 });
 
@@ -22,4 +24,62 @@ function list_available_devices() {
 
 function list_registered_devices() {
 
+}
+
+async function scan_lan_devices() {
+	const networkPrefix = "192.168.254.";
+	const startIP = 1;
+	const endIP = 255;
+	let devices = [];
+
+	console.log('Scanning network...');
+
+	const promises = [];
+
+	for (let i = startIP; i <= endIP; i++) {
+		const host = networkPrefix + i;
+		const promise = ping.promise.probe(host);
+		promises.push(promise);
+	}
+
+	const results = await Promise.all(promises);
+
+	for (let i = 0; i < results.length; i++) {
+		if (results[i].alive) {
+			const ip = networkPrefix + (i + startIP);
+
+			const deviceNamePromise = new Promise((resolve, reject) => {
+				dns.reverse(ip, (err, hostnames) => {
+					if (!err && hostnames && hostnames.length > 0) {
+						resolve(hostnames[0]);
+					} else {
+						resolve("Unknown");
+					}
+				});
+			});
+			// Await the DNS lookup Promise
+			const deviceName = await deviceNamePromise;
+
+			// store ip and device name on devices
+			devices.push({ ip, deviceName });
+		}
+	}
+
+	console.log(devices);
+
+	let placeholder = document.querySelector("#local_devices");
+	let out = "";
+
+	for(let row of devices) {
+		out += `
+			<tr>
+				<td>${row.deviceName}</td>
+				<td>${row.ip}</td>
+			</tr>
+		`;
+	}
+
+	placeholder.innerHTML = out;
+
+	console.log('Scan completed.');
 }
