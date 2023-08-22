af880393d98adb84a3062c36df9c540e25424bde
alexxShandsome <algilbertgomez@gmail.com>
2023-08-22 11:49:20 +0800

    refactor: remove unnecessary database connection checks

--- Manage/public/js/inventory.js
+++ Manage/public/js/inventory.js
@@ -13,58 +13,19 @@ const connection = mysql.connection;
 // check database connection
 mysql.check_connection();
 
-// SQL Connection Test
-function testsql()
-{
-	connection.connect((err) => {
-		if (err) {
-			return console.log(err.stack);
-		}
-		console.log("Connection Success");
-	});
-
-	// Queries Will be executed //
-
-	$queryString = 'SELECT * FROM manage_db.menu_items;';
-	connection.query($queryString, (err, rows, fields) => {
-
-		if(err) {
-			return console.log(err.stack)
-		}
-
-		console.log(rows);
-
-	});
-
-	// ------------------------ //
-
-	connection.end(() => {
-		console.log("Connection Closed");
-	});
-}
-// END OF SQL Connection Test
-
 // Function for Refresh Items //
-function refreshitems()
-{
-	connection.connect((err) => {
-		if (err) {
-			return console.log(err.stack);
-		}
-		console.log("Connection Success");
-	});
-
+function refreshitems() {
 	// Start Query Here
 	connection.connect(function(err) {
 		if (err) throw err;
-  		//Select all customers and return the result object:
-  		connection.query("SELECT * FROM manage_db.menu_items", function (err, result, fields) {
-    		if (err) throw err;
+		//Select all customers and return the result object:
+		connection.query("SELECT * FROM manage_db.menu_items", function(err, result, fields) {
+			if (err) throw err;
 
 			let placeholder = document.querySelector("#data-output");
 			let out = "";
 
-			for(let row of result) {
+			for (let row of result) {
 				out += `
 					<tr>
 						<td>${row.item_id}</td>
@@ -80,34 +41,23 @@ function refreshitems()
 
 			placeholder.innerHTML = out;
 
-  		});
-
+		});
 	})
-
 }
 // End of Function for Refresh Item //
 
-// END OF MYSQL FUNCTIONS //
-
 //----- Show Foods Function -----//
-connection.connect((err) => {
-	if (err) {
-		return console.log(err.stack);
-	}
-	console.log("Connection Success");
-});
-
 // Start Query Here
 connection.connect(function(err) {
 	if (err) throw err;
 	//Select all foods and return the result object:
-	connection.query("SELECT * FROM manage_db.menu_items", function (err, result, fields){
+	connection.query("SELECT * FROM manage_db.menu_items", function(err, result, fields) {
 		if (err) throw err;
 
 		let placeholder = document.querySelector("#data-output");
 		let out = "";
 
-		for(let row of result){
+		for (let row of result) {
 			out += `
 				<tr>
 					<td>${row.item_id}</td>
@@ -123,23 +73,13 @@ connection.connect(function(err) {
 
 		placeholder.innerHTML = out;
 
-	  });
-
+	});
 })
 
 //----- End of Show Foods Function -----//
 
 // -----ADD NEW FOODS FUNCTION----- //
-function addItems()
-{
-
-	// SQL CONNECTION
-	connection.connect((err) => {
-		if (err) {
-			return console.log(err.stack);
-		}
-		console.log("Connection Success");
-	});
+function addItems() {
 
 	// FUNCTION TO INSERT
 	connection.connect(function(err) {
@@ -169,15 +109,16 @@ function addItems()
 		// THE QUERY USED TO INSERT
 		const query = 'INSERT INTO menu_items (item_name, item_desc, item_image, item_price) VALUES (?, ?, ?, ?);';
 		connection.query(query, [fooditem, fooddesc, withimg, foodprice], (error, results) => {
-			if(error){
+			if (error) {
 				alert(error);
-			}else{
+			} else {
 				alert('Data Inserted Successfully!');
+				//refresh page after insert
+				location.reload();
 			}
 		});
 
 	})
 
 }
-
 // -----END OF ADD NEW FOODS FUNCTION----- //
