ab2acd245acfae4605e5318993dcde0ff2b2e9cc
Kenneth <kennethseva09@gmail.com>
2023-09-04 20:12:41 +0800

    add update and delete function for registering employee

--- Manage/public/js/registration.js
+++ Manage/public/js/registration.js
@@ -98,6 +98,7 @@ function list_registered_employees() {
 
 			out += `
 				<tr>
+					<td>${row.employee_id}</td>
 					<td>${row.name}</td>
 					<td>${row.design_priv}</td>
 					<td>${row.inventory_priv}</td>
@@ -108,3 +109,131 @@ function list_registered_employees() {
 		placeholder.innerHTML = out;
 	});
 }
+
+// TABLE CLICK FUNCTION //
+function rowClick()
+{
+	let design_privilege = document.getElementById("design_priv");
+	let inventory_privilege = document.getElementById("inventory_priv");
+	let report_privilege = document.getElementById("report_priv");
+
+	var table = document.getElementById("employee_table");
+	var rows = table.getElementsByTagName("tr");
+	for(i = 0; i < rows.length; i++)
+	{
+		var currentRow = table.rows[i];
+		var clickHandle = 
+			function(row)
+			{
+				return function()
+				{
+					var employee_id = row.getElementsByTagName("td")[0];
+					var id = employee_id.innerHTML;
+
+					var employee_name = row.getElementsByTagName("td")[1];
+					var name = employee_name.innerHTML;
+
+					var design_priv = row.getElementsByTagName("td")[2];
+					var design = design_priv.innerHTML;
+
+					var inventory_priv = row.getElementsByTagName("td")[3];
+					var inventory = inventory_priv.innerHTML;
+
+					var reports_priv = row.getElementsByTagName("td")[4];
+					var reports = reports_priv.innerHTML;
+
+					document.getElementById("id").value = id;
+					document.getElementById("name").value = name;
+
+					if (design == "Yes")
+						design_privilege.checked = true;
+					else
+						design_privilege.checked = false;
+
+					if (inventory == "Yes")
+						inventory_privilege.checked = true;
+					else
+						inventory_privilege.checked = false;
+
+					if (reports == "Yes")
+						report_privilege.checked = true;
+					else
+						report_privilege.checked = false;
+
+				};
+			};
+
+		currentRow.onclick = clickHandle(currentRow);
+	}
+}
+// END OF TABLE CLICK FUNCTION //
+
+// UPDATE EMPLOYEE FUNCTION
+function update_employee()
+{
+	// hidden item input for id
+	var id = document.getElementById("id").value;
+
+	// variables from register.html
+	var name = document.getElementById("name").value.trim();
+	// cancel if input box is empty
+	if (name.length == 0 || name.trim() === "")
+		return alert("Name is empty");
+	
+	// privilege variables
+	// assign 1 if the corresponding checkbox id is checked
+	var design_priv = 0;
+	if (document.querySelector("#design_priv").checked == true)
+		design_priv = 1;
+
+	var inventory_priv = 0;
+	if (document.querySelector("#inventory_priv").checked == true)
+		inventory_priv = 1;
+
+	var report_priv = 0;
+	if (document.querySelector("#report_priv").checked == true)
+		report_priv = 1;
+
+	const query = `UPDATE registered_employees SET name = "${name}", design_priv = "${design_priv}", inventory_priv = "${inventory_priv}", view_reports_priv = "${report_priv}" WHERE employee_id = "${id}"`;
+	connection.query(query, function(error, results) {
+		if(error)
+		{
+			alert("Some fields are empty");
+			console.log(error);
+		}
+		else
+		{
+			alert("Employee updated!!");
+			location.reload();
+		}
+	});
+
+}
+// END OF UPDATE EMPLOYEE FUNCTION //
+
+// DELETE EMPLOYEE FUNCTION
+function delete_employee()
+{
+	// hidden item input for id
+	var id = document.getElementById("id").value;
+	if (id.length == 0)
+		return alert("Select a employee first!");
+
+	connection.connect(function (err) {
+		const query = `DELETE FROM registered_employees WHERE employee_id = "${id}"`;
+		connection.query(query, function(error, results) {
+			if(error)
+			{
+				alert("Select an item first!")
+				console.log(error)
+			}
+			else
+			{
+				alert('Employee has been removed!')
+				location.reload();
+			}
+		})
+	})
+
+}
+// END OF UPDATE EMPLOYEE FUNCTION //
