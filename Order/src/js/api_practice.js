7032ba14c09f69cb6cea832c14cbc0b1fe11729a
alexxShandsome <algilbertgomez@gmail.com>
2023-09-11 13:54:06 +0800

    order: fetch data from menu_items and pull to refresh function

--- Order/src/js/api_practice.js
+++ Order/src/js/api_practice.js
@@ -1,12 +1,21 @@
-console.log("Directory: " + __dirname);
+import { CapacitorHttp } from '@capacitor/core';
 
-const url = '192.168.254.115:8080/menu_items';
+function get_request_menu_items() {
+	const request_promise = CapacitorHttp.get(
+		{
+			url: "http://192.168.254.115:8080/menu_items",
+		}
+	).then(response => {
+		console.log("Response Status: " + response.status);
+		console.log(response.data);
 
-fetch(url)
-	.then((response) => {
-		response.json();
-		response.status;
-	})
-	.then((data) => {
-		console.log(data);
+		// display data in the html page
+		const display_data = JSON.stringify(response.data, null, 2)
+		const json_container = document.getElementById("menu_items");
+		json_container.textContent = display_data;
+	}).catch(error => {
+		console.error("An error occurred: ", error)
 	});
+}
+
+get_request_menu_items();
---
+++ Order/src/js/pull_to_refresh.js
@@ -0,0 +1,19 @@
+const pullToRefresh = document.querySelector('.pull-to-refresh');
+let touchstartY = 0;
+document.addEventListener('touchstart', e => {
+	touchstartY = e.touches[0].clientY;
+});
+document.addEventListener('touchmove', e => {
+	const touchY = e.touches[0].clientY;
+	const touchDiff = touchY - touchstartY;
+	if (touchDiff > 0 && window.scrollY === 0) {
+		pullToRefresh.classList.add('visible');
+		e.preventDefault();
+	}
+});
+document.addEventListener('touchend', e => {
+	if (pullToRefresh.classList.contains('visible')) {
+		pullToRefresh.classList.remove('visible');
+		location.reload();
+	}
+});
