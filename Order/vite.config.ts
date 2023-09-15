8543573285dc941dd051c28e06ce6ec3b0bf1ac5
alexxShandsome <algilbertgomez@gmail.com>
2023-09-15 19:44:30 +0800

    order: heehee

--- Order/vite.config.ts
+++ Order/vite.config.ts
@@ -1,17 +1,16 @@
-import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: './src',
-  build: {
-    outDir: '../dist/',
-    minify: false,
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-		    main: "./src/index.html",
-		    heehee: "./src/heehee.html",
-      }
-    }
-  },
+	root: './src',
+	build: {
+		outDir: '../dist/',
+		minify: false,
+		emptyOutDir: true,
+		rollupOptions: {
+			input: {
+				main: "./src/index.html",
+				heehee: "./src/heehee.html",
+			}
+		}
+	},
 });
