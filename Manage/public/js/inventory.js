df6eff261ccbe3999e4009de1aa2bdcc820aaa73
Kenneth <kennethseva09@gmail.com>
2023-09-18 13:43:49 +0800

    inventory.html design complete (might need some adjustments)

--- Manage/public/js/inventory.js
+++ Manage/public/js/inventory.js
@@ -34,8 +34,15 @@ function refreshitems() {
 					<td class="text-center">${row.revenue_generated}</td>
 					<td>
 						<span class="action-btn">
-							<a onclick="showDialog()">Edit</button>
-							<a onclick="deleteItems()">Remove</a>
+							<button class="rounded-lg bg-sky-400 py-2 px-2 inline-flex hover:bg-sky-600 text-zinc-50 hover:drop-shadow-lg" onclick="showDialog()">
+								<svg viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6"><path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32l8.4-8.4z" />
+								<path d="M5.25 5.25a3 3 0 00-3 3v10.5a3 3 0 003 3h10.5a3 3 0 003-3V13.5a.75.75 0 00-1.5 0v5.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5V8.25a1.5 1.5 0 011.5-1.5h5.25a.75.75 0 000-1.5H5.25z" />
+								</svg>
+							</button>
+							<button class="rounded-lg bg-rose-500 py-2 px-2 inline-flex hover:bg-rose-700 text-zinc-50 hover:drop-shadow-lg" onclick="deleteItems()">
+								<svg viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6"><path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z" clip-rule="evenodd" />
+								</svg>
+							</button>
 						</span>
 					</td>
 				</tr>
@@ -69,8 +76,15 @@ connection.connect(function(err) {
 					<td class="text-center">${row.revenue_generated}</td>
 					<td>
 						<span class="action-btn">
-							<a onclick="showDialog()">Edit</button>
-							<a onclick="deleteItems()">Remove</a>
+						<button class="rounded-lg bg-sky-400 py-2 px-2 inline-flex hover:bg-sky-600 text-zinc-50 hover:drop-shadow-lg" onclick="showDialog()">
+							<svg viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6"><path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32l8.4-8.4z" />
+							<path d="M5.25 5.25a3 3 0 00-3 3v10.5a3 3 0 003 3h10.5a3 3 0 003-3V13.5a.75.75 0 00-1.5 0v5.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5V8.25a1.5 1.5 0 011.5-1.5h5.25a.75.75 0 000-1.5H5.25z" />
+							</svg>
+						</button>
+						<button class="rounded-lg bg-rose-500 py-2 px-2 inline-flex hover:bg-rose-700 text-zinc-50 hover:drop-shadow-lg" onclick="deleteItems()">
+							<svg viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6"><path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z" clip-rule="evenodd" />
+							</svg>
+						</button>
 						</span>
 					</td>
 				</tr>
@@ -88,7 +102,7 @@ connection.connect(function(err) {
 function addItems() {
 	// CHECKS IF FILE INPUT IS EMPTY OR NOT
 	if (document.getElementById("foodimg").files.length == 0) {
-		alert("Select a Image first!")
+		errormodal();
 	}
 	else {
 		// VARIABLES FROM inventory.html
@@ -116,12 +130,9 @@ function addItems() {
 		const query = 'INSERT INTO menu_items (item_name, item_desc, item_image, item_price) VALUES (?, ?, ?, ?);';
 		connection.query(query, [fooditem, fooddesc, withimg, foodprice], (error, results) => {
 			if (error) {
-				alert("Some input fields are empty!");
-				console.log(error);
+				errormodal();
 			} else {
-				alert(fooditem + " Inserted Successfully!");
-				//refresh page after insert
-				location.reload();
+				addsuccessmodal();
 			}
 		});
 	}
@@ -170,7 +181,7 @@ function rowClick() {
 function updateItems() {
 	// CHECKS IF FILE INPUT IS EMPTY OR NOT
 	if (document.getElementById("foodimg_2").files.length == 0) {
-		alert("Select a Image first!")
+		errormodal();
 	}
 	else {
 		// VARIABLES FROM inventory.html
@@ -201,12 +212,13 @@ function updateItems() {
 		connection.query(query, function(error, results) {
 			if (error) {
 				console.log(error);
-				alert("Some input fields are empty!")
+				errormodal();
 			}
 			else {
-				alert("Item Updated Successfully!");
-				//refresh page after insert
-				location.reload();
+				// alert("Item Updated Successfully!");
+				// //refresh page after insert
+				// location.reload();
+				updateitemsuccess();
 			}
 		});
 
@@ -237,8 +249,7 @@ function deleteItems() {
 							console.log(error)
 						}
 						else {
-							alert(name + " has been removed!")
-							location.reload();
+							removesuccessopen();
 						}
 					})
 				};
@@ -262,7 +273,7 @@ function closeModal() {
 	favDialog.close();
 }
 
-// add item modal (open and close)
+// add item modal (open, close, success and error)
 function additemdialogopen() {
 	const dialog = document.getElementById("addItemsModal");
 	dialog.showModal();
@@ -273,3 +284,46 @@ function additemdialogclose() {
 	dialog.close();
 }
 
+function addsuccessmodal() {
+	const dialog = document.getElementById("addsuccess");
+	document.getElementById("item_name_placeholder").innerHTML = document.getElementById("fooditem").value;
+	dialog.showModal();
+}
+
+function additemsuccessclose() {
+	location.reload();
+}
+
+function errormodal() {
+	const dialog = document.getElementById("errordialog");
+	dialog.showModal();
+}
+
+function errorclose() {
+	const dialog = document.getElementById("errordialog")
+	dialog.close();
+}
+
+// update item modal (success and close)
+
+function updateitemsuccess() {
+	const dialog = document.getElementById("updatesuccess");
+	document.getElementById("item_name_placeholder_3").innerHTML = document.getElementById("fooditem_2").value;
+	dialog.showModal();
+}
+
+function updateitemsuccessclose() {
+	location.reload();
+}
+
+// remove item modal (remove item)
+
+function removesuccessopen() {
+	const dialog = document.getElementById("removemodal");
+	document.getElementById("item_name_placeholder_2").innerHTML = document.getElementById("fooditem_2").value;
+	dialog.showModal();
+}
+
+function removesuccessclose() {
+	location.reload();
+}
