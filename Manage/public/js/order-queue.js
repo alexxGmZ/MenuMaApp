7d03b4c760a0f2b5c2ad79a7260d4f72f6340f65
alexxShandsome <algilbertgomez@gmail.com>
2023-09-18 18:39:05 +0800

    manage: refactor main.html, move script to order_queue.js

---
+++ Manage/public/js/order-queue.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function() {
+	display_orders();
+});
+
+function display_orders() {
+	fetch('http://localhost:8080/orders')
+		.then(res => {
+			return res.json();
+		})
+		.then(data => {
+			console.log(data)
+			data.forEach(orders => {
+
+				const food_items = orders.items_ordered.map((item) => {
+					return `${item.quantity} ${item.item_name}`;
+				})
+				console.log(food_items);
+
+				const markup = `
+					<td>
+						<table class="border-separate border-spacing-2 border border-slate-500 rounded-lg">
+						<td><h1 style="padding-top: 5px; padding-bottom: 5px;">Order #${orders.queue_number}</h1></td>
+						<tr>
+						<td>
+							<ul>
+								<li>${food_items}</li>
+							</ul>
+						</td>
+						</tr>
+						<tr>
+						<td>Customer: ${orders.customer_name}</td>
+						</tr>
+						<tr>
+						<td style="padding-top: 5px; padding-bottom: 5px;">Total: ₱${orders.total_price}</td>
+						</table>
+					</td>
+				`;
+				document.querySelector("#data-output").insertAdjacentHTML('beforeend', markup);
+			})
+		})
+		.catch(error => console.log(error));
+}
+
