1acfa09d70db92c2dbaf023502910a8e3b8327ba
Kenneth <kennethseva09@gmail.com>
2023-10-02 13:06:42 +0800

    add a design in queue completed (next is prompt and dialogs)

--- Manage/public/js/order-queue.js
+++ Manage/public/js/order-queue.js
@@ -26,7 +26,7 @@ function display_orders() {
 						<tr>
 						<td>
 							<ul>
-								<li><textarea disabled class="h-48 pl-2 bg-white rounded-lg">${removed_comma}</textarea></li>
+								<li><textarea disabled class="h-56 pl-2 bg-white rounded-lg" style="resize: none;">${removed_comma}</textarea></li>
 							</ul>
 						</td>
 						</tr>
@@ -35,6 +35,26 @@ function display_orders() {
 						</tr>
 						<tr>
 						<td style="padding-top: 5px; padding-bottom: 5px;">Total: ₱${orders.total_price}</td>
+						</table>
+						<table>
+						<ul>
+							<li>
+							<center><button onclick="location.reload()" class="font-bold rounded-full bg-green-500 mt-2 py-2 px-2 hover:text-zinc-50 hover:drop-shadow-lg w-3/4 flex items-center justify-center">
+								<svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+								<path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+								</svg>
+								<span class="mx-2"> DONE </span>
+							</button></center>
+							</li>
+							<li>
+							<center><button onclick="location.reload()" class="font-bold rounded-full bg-red-500 mt-2 py-2 px-2 hover:text-zinc-50 hover:drop-shadow-lg w-3/4 flex items-center justify-center">
+								<svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+								<path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+								</svg>
+								<span class="mx-2"> CANCEL </span>
+							</button></center>
+							</li>
+						</ul>
 						</table>
 					</td>
 				`;
