5fcaac2e52a14504c510141e10b3a9cdf77c93a3
alexxShandsome <algilbertgomez@gmail.com>
2023-10-04 10:16:29 +0800

    order-queue: did some fucking proper indention, coz lots of people are going crazy in css coz they go absolute bonkers on html tags

--- Manage/public/js/order-queue.js
+++ Manage/public/js/order-queue.js
@@ -21,36 +21,39 @@ function display_orders() {
 
 				const markup = `
 					<td>
-						<table class="border-separate border-spacing-2 border border-slate-500 rounded-lg bg-amber-200 rounded-lg">
-						<td><center><h1 class="font-bold">Order #${orders.queue_number}</h1></center></td>
-						<tr>
-						<td>
-							<ul>
-								<li><textarea disabled class="h-56 pl-2 bg-white rounded-lg" style="resize: none;">${removed_comma}</textarea></li>
-							</ul>
-						</td>
-						</tr>
-						<tr>
-						<td>Customer: ${orders.customer_name}</td>
-						</tr>
-						<tr>
-						<td style="padding-top: 5px; padding-bottom: 5px;">Total: ₱${orders.total_price}</td>
+						<table class="border-separate border-spacing-2 border border-slate-500 rounded-lg bg-amber-200">
+							<td><center><h1 class="font-bold">Order #${orders.queue_number}</h1></center></td>
+								<tr>
+									<td>
+										<ul>
+											<li><textarea disabled class="h-56 pl-2 bg-white rounded-lg" style="resize: none;">${removed_comma}</textarea></li>
+										</ul>
+									</td>
+								</tr>
+								<tr>
+									<td>Customer: ${orders.customer_name}</td>
+								</tr>
+							<td style="padding-top: 5px; padding-bottom: 5px;">Total: ₱${orders.total_price}</td>
 						</table>
 						<table>
-						<ul>
-							<li>
-							<center><button onclick="location.reload()" class="font-bold rounded-full bg-green-500 mt-2 py-2 px-2 hover:text-zinc-50 hover:drop-shadow-lg w-3/4 flex items-center justify-center">
-								<img src="assets/svg/check-circle.svg" class="hover:text-zinc-50">
-								<span class="mx-2"> DONE </span>
-							</button></center>
-							</li>
-							<li>
-							<center><button onclick="location.reload()" class="font-bold rounded-full bg-red-500 mt-2 py-2 px-2 hover:text-zinc-50 hover:drop-shadow-lg w-3/4 flex items-center justify-center">
-								<img src="assets/svg/x-circle.svg" class="hover:text-zinc-50">
-								<span class="mx-2"> CANCEL </span>
-							</button></center>
-							</li>
-						</ul>
+							<ul>
+								<li>
+									<center>
+										<button onclick="location.reload()" class="font-bold rounded-full bg-green-500 mt-2 py-2 px-2 hover:text-zinc-50 hover:drop-shadow-lg w-3/4 flex items-center justify-center">
+											<img src="assets/svg/check-circle.svg" class="hover:text-zinc-50">
+											<span class="mx-2"> DONE </span>
+										</button>
+									</center>
+								</li>
+								<li>
+									<center>
+										<button onclick="location.reload()" class="font-bold rounded-full bg-red-500 mt-2 py-2 px-2 hover:text-zinc-50 hover:drop-shadow-lg w-3/4 flex items-center justify-center">
+											<img src="assets/svg/x-circle.svg" class="hover:text-zinc-50">
+											<span class="mx-2"> CANCEL </span>
+										</button>
+									</center>
+								</li>
+							</ul>
 						</table>
 					</td>
 				`;
@@ -60,3 +63,10 @@ function display_orders() {
 		.catch(error => console.log(error));
 }
 
+function order_done() {
+
+}
+
+function order_cancel() {
+
+}
