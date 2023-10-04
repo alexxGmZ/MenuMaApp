abaa63f4e7d50dc2ad26ddba77d8831941e0f891
Kenneth <kennethseva09@gmail.com>
2023-10-04 09:32:17 +0800

    add .svg files and replace current icon to .svg file directly

--- Manage/public/js/order-queue.js
+++ Manage/public/js/order-queue.js
@@ -40,17 +40,13 @@ function display_orders() {
 						<ul>
 							<li>
 							<center><button onclick="location.reload()" class="font-bold rounded-full bg-green-500 mt-2 py-2 px-2 hover:text-zinc-50 hover:drop-shadow-lg w-3/4 flex items-center justify-center">
-								<svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-								<path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-								</svg>
+								<img src="assets/svg/check-circle.svg" class="hover:text-zinc-50">
 								<span class="mx-2"> DONE </span>
 							</button></center>
 							</li>
 							<li>
 							<center><button onclick="location.reload()" class="font-bold rounded-full bg-red-500 mt-2 py-2 px-2 hover:text-zinc-50 hover:drop-shadow-lg w-3/4 flex items-center justify-center">
-								<svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-								<path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-								</svg>
+								<img src="assets/svg/x-circle.svg" class="hover:text-zinc-50">
 								<span class="mx-2"> CANCEL </span>
 							</button></center>
 							</li>
