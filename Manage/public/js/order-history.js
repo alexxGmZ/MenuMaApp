60b4853eda744de5e2c898650704aae561d3f4ff
Kenneth <kennethseva09@gmail.com>
2023-10-19 14:27:29 +0800

    add function on history to remove

--- Manage/public/js/order-history.js
+++ Manage/public/js/order-history.js
@@ -77,6 +77,7 @@ function display_order_history() {
 
 					const markup =`
 						<tr class="bg-white border-b dark:border-gray-700 border-r border-l ${rowColors}">
+							<td class="px-2">${order_history.order_id}</td>
 							<td class="px-2">Order #${order_history.queue_number}</td>
 							<td class="">${order_history.customer_name}</td>
 							<td class="whitespace-pre py-3">${removed_comma}</td>
@@ -85,7 +86,7 @@ function display_order_history() {
 							<td class="text-center"><p class="rounded-full font-bold py-2 mx-4 ${statusClass}">${order_history.order_status}</p></td>
 							<td>
 								<span class="action-btn">
-									<center><button onclick="" class="rounded-lg bg-rose-500 py-2 px-2 inline-flex hover:bg-rose-300 text-zinc-50 hover:drop-shadow-lg">
+									<center><button onclick="dialog_open('remove_item_dialog'); row_click()" class="rounded-lg bg-rose-500 py-2 px-2 inline-flex hover:bg-rose-300 text-zinc-50 hover:drop-shadow-lg">
 										<img src="assets/svg/trash.svg" class="hover:text-zinc-50">
 									</button></center>
 								</span>
@@ -111,9 +112,93 @@ function display_order_history() {
 	})
 }
 
+function row_click() {
+	// Find the clicked Row
+	var table = document.getElementById("order_history_table");
+	var rows = table.getElementsByTagName("tr");
+
+	for (let i = 0;  i < rows.length; i++) {
+		var currentRow = table.rows[i];
+		var clickHandle = function(row) {
+			return function() {
+				var history_id = row.getElementsByTagName("td")[0];
+				var history_num_id = history_id.innerHTML;
+
+				var queue_num = row.getElementsByTagName("td")[1];
+				var queue_order_num = queue_num.innerHTML;
+
+				var customer_name = row.getElementsByTagName("td")[2];
+				var customers = customer_name.innerHTML;
+
+				var items_ordered = row.getElementsByTagName("td")[3];
+				var items = items_ordered.innerHTML;
+				
+				var total_price = row.getElementsByTagName("td")[4];
+				var prices = total_price.innerHTML;
+
+				var transaction_Date = row.getElementsByTagName("td")[5];
+				var t_date = transaction_Date.innerHTML;
+
+				var history_status = row.getElementsByTagName("td")[6];
+				var status = history_status.innerHTML;
+
+				//console.log("Here the history clicked:")
+				//console.log(history_num_id + "\n" + queue_order_num + "\n" + customers + "\n" + items + "\n" + prices + "\n" + t_date + "\n" + status)
+
+				document.getElementById('history_id').value = history_num_id;
+				document.getElementById('history_queue_number').value = queue_order_num;
+				document.getElementById('history_customer').value = customers;
+				document.getElementById('history_items').value = items;
+				document.getElementById('history_price').value = prices;
+				document.getElementById('history_date').value = t_date;
+				document.getElementById('history_status').innerHTML = status;
+
+
+			};
+		};
+		currentRow.onclick = clickHandle(currentRow);
+	}
+	
+}
+
+function remove_history() {
+
+	var history_id = document.getElementById('history_id').value;
+	console.log(history_id);
+
+	// queries fro deleting history
+	const items_ordered_query = `DELETE FROM items_ordered_history WHERE order_id = "${history_id}"`;
+	const ordered_num_query = `DELETE FROM order_queue_history WHERE order_id = "${history_id}"`;
+
+	connection.query(ordered_num_query, error => {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log("Removed Success from order_queue_history");
+
+			connection.query(items_ordered_query, error => {
+				if (error) {
+					console.log(error);
+				} else {
+					console.log("Removed Success from items_ordered-history");
+					dialog_open('history_remove_success_dialog');
+				}
+			})
+		}
+	})
+
+	
+
+
+}
+
 // open modal dialog based on element id
 function dialog_open(element_id) {
     const fav_dialog = document.getElementById(element_id);
+
+	if (element_id == "history_remove_success_dialog") {
+		document.getElementById("history_id_remove_placeholder").innerHTML = document.getElementById('history_id').value;
+	}
 
     fav_dialog.showModal();
 }
