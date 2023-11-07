5cad832df7849e58f38837a3fb00824a458abab6
alexxShandsome <algilbertgomez@gmail.com>
2023-11-07 10:35:14 +0800

    MANAGE(refactor): dialog_open() and dialog_close() is now a module

---
+++ Manage/public/js/modules/dialog.js
@@ -0,0 +1,18 @@
+function dialog_open(element_id) {
+	console.log(`called dialog_open(${element_id})`);
+	const fav_dialog = document.getElementById(element_id);
+	fav_dialog.classList.add("active-dialog");
+	fav_dialog.showModal();
+}
+
+function dialog_close(element_id) {
+	console.log(`called dialog_close(${element_id})`);
+	const fav_dialog = document.getElementById(element_id);
+	fav_dialog.classList.remove("active-dialog");
+	fav_dialog.close();
+}
+
+module.exports = {
+	dialog_open,
+	dialog_close
+}
