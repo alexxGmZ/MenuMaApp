cea76c7ac4623d94aec3629d1763cd865730ff72
alexxShandsome <algilbertgomez@gmail.com>
2023-11-13 17:32:15 +0800

    MANAGE-designer(refactor): use copy and paste code from fabric.js documentation

--- Manage/public/js/designer.js
+++ Manage/public/js/designer.js
@@ -423,35 +423,58 @@ function delete_selected_objects() {
 	}
 }
 
-let copied_objects = [];
+let _clipboard = null;
 function copy_selected_objects() {
 	if (!canvas) return;
 	console.log("called copy_selected_objects()");
 
-	const selected_objects = canvas.getActiveObjects();
-	copied_objects = [];
-
-	selected_objects.forEach(object => {
-		const cloned_object = fabric.util.object.clone(object);
-		cloned_object.set({
-			left: object.left + 10, // Offset to prevent pasted objects from overlapping
-			top: object.top + 10,
-		});
-		copied_objects.push(cloned_object);
-	})
+	canvas.getActiveObject().clone(function(cloned) {
+		_clipboard = cloned;
+		// Log the copied object or objects
+		if (cloned.type === 'activeSelection') {
+			// Iterate through each object in the active selection
+			cloned.forEachObject(function(obj) {
+				console.log(`Copied object - Type: ${obj.type}, Object ID: ${obj.object_id}`);
+			});
+		}
+		else {
+			// Log the single copied object
+			console.log(`Copied object - Type: ${cloned.type}, Object ID: ${cloned.object_id}`);
+		}
+	});
 }
 
 function paste_copied_objects() {
 	if (!canvas) return;
 	console.log("called paste_copied_objects()");
 
-	if (copied_objects.length > 0) {
-		copied_objects.forEach(object => {
-			canvas.add(object);
-			canvas.setActiveObject(object);
-		})
+	// clone again, so you can do multiple copies.
+	_clipboard.clone(function(clonedObj) {
+		canvas.discardActiveObject();
+		clonedObj.set({
+			left: clonedObj.left + 10,
+			top: clonedObj.top + 10,
+			evented: true,
+		});
+		if (clonedObj.type === 'activeSelection') {
+			// active selection needs a reference to the canvas.
+			clonedObj.canvas = canvas;
+			clonedObj.forEachObject(function(obj) {
+				canvas.add(obj);
+				console.log(`Pasted object - Type: ${obj.type}, Object ID: ${obj.object_id}`);
+			});
+			// this should solve the unselectability
+			clonedObj.setCoords();
+		}
+		else {
+			canvas.add(clonedObj);
+			console.log(`Pasted object - Type: ${clonedObj.type}, Object ID: ${clonedObj.object_id}`);
+		}
+		_clipboard.top += 10;
+		_clipboard.left += 10;
+		canvas.setActiveObject(clonedObj);
 		canvas.requestRenderAll();
-	}
+	});
 }
 
 function generate_random_num() {
