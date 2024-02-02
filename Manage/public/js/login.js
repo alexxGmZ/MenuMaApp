9b746d396a52489ee86557f22de1eadaf7ea53e7
alexxShandsome <algilbertgomez@gmail.com>
2024-02-02 18:30:38 +0800

    MANAGE-refactor: start editing for revision

---
+++ Manage/public/js/login.js
@@ -0,0 +1,5 @@
+// TODO: create a login session and a logout functionality
+
+const mysql = require(__dirname + "/js/modules/mysql.js");
+const connection = mysql.connection;
+mysql.check_connection();
