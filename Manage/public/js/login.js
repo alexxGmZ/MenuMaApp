c77f2da78086b40ceff230c8301c0ba9c9ff3ee4
alexxShandsome <algilbertgomez@gmail.com>
2024-02-04 10:47:55 +0800

    feat: store employee credentials in sessionStorage

--- Manage/public/js/login.js
+++ Manage/public/js/login.js
@@ -1,4 +1,7 @@
-// TODO: create a login session and a logout functionality
+// TODO:
+// (/) create a login session and a logout functionality.
+// ( ) create a proper page redirection when an employee is successfully logged in
+//     based on the page clicked and their corresponding access rights.
 
 function login_dialog_open(redirect_site) {
 	console.log(`called login_dialog_open(${redirect_site})`)
@@ -8,7 +11,6 @@ function login_dialog_open(redirect_site) {
 	// check if an employee is logged in
 	if (!employee_name || employee_name === "") {
 		console.log("not logged in");
-		// dialog_open("login_dialog");
 		const fav_dialog = document.getElementById("login_dialog");
 		fav_dialog.classList.add("active-dialog");
 		fav_dialog.classList.remove("hidden");
@@ -30,67 +32,45 @@ function login_dialog_open(redirect_site) {
 	}
 	else {
 		console.log("logged in");
-		location.replace(redirect_site);
+		if (!redirect_site || redirect_site === "")
+			location.replace("main.html");
+		else
+			location.replace(redirect_site);
 	}
 }
 
 function login_user() {
 	console.log("called login_user()");
-
 	const login_username = document.getElementById("login_username").value.trim();
 	const login_password = document.getElementById("login_password").value;
 	const login_password_hash = crypto.createHash('sha256').update(login_password).digest('hex');
-	// console.log(login_username, login_password, login_password_hash);
-
 	const redirect_site = document.getElementById("login_redirect_site").textContent;
-	console.log("redirect_site", redirect_site);
-	const feature_privilege_map = {
-		"inventory.html": "inventory_priv",
-		"registration.html": "manage_employee_priv",
-		"kiosk-devices.html": "manage_devices_priv",
-		"order-history.html": "view_reports_priv",
-		"designer.html": "design_priv",
-	}
-	let feature_privilege = feature_privilege_map[redirect_site];
-	console.log("feature_privilege", feature_privilege);
 
-	let query = `SELECT name, password_hash, ${feature_privilege} FROM registered_employees WHERE name = "${login_username}"`;
-	if (!feature_privilege || feature_privilege === "")
-		query = `SELECT name, password_hash FROM registered_employees WHERE name = "${login_username}"`;
-
+	const query = `select * from manage_db.registered_employees where name = "${login_username}"`;
 	connection.query(query, (error, result) => {
 		if (error) throw error;
 
-		const user_data = result;
-
-		// Error function if the username didn't exists
-		if (user_data.length === 0) {
+		const employee_data = result;
+		if (employee_data.length === 0) {
 			dialog_open('login_invalid_username_dialog');
 		}
-
-		if (user_data.length > 0) {
-			const user_row = user_data[0];
-			const password = user_row.password_hash;
-			const converted_hash_password = Buffer.from(password).toString('utf8');
-			const feature_priv_status = user_row[feature_privilege];
-
-			if (converted_hash_password === login_password_hash) {
-				if (feature_priv_status === 1) {
-					console.log("logged in");
-					location.replace(redirect_site);
+		else {
+			const employee_row = employee_data[0];
+			const user_password_hash = Buffer.from(employee_row.password_hash).toString("utf8");
+			if (user_password_hash === login_password_hash) {
+				console.log("logged in");
+				const employee_rights = {
+					"designer": employee_row.design_priv,
+					"menu_items": employee_row.inventory_priv,
+					"reports": employee_row.view_reports_priv,
+					"manage_users": employee_row.manage_employee_priv,
+					"manage_devices": employee_row.manage_devices_priv
 				}
-				else {
-					if (!redirect_site || redirect_site === "") {
-						console.log("logged in");
-						location.replace(window.location.href);
-					}
-					else {
-						console.log("not logged in");
-						dialog_open('lack_access_privilege_dialog');
-					}
-					// clear password input box
-					document.getElementById("login_password").value = "";
-				}
+				console.log("employee_rights", employee_rights);
+
+				// store employee credentials to session variables
+				sessionStorage.setItem("employee_name", employee_row.name);
+				sessionStorage.setItem("employee_rights", JSON.stringify(employee_rights));
 			}
 			else {
 				dialog_open('login_invalid_password_dialog');
@@ -102,5 +82,8 @@ function login_user() {
 }
 
 function logout_user() {
-
+	console.log("called logout_user()");
+	sessionStorage.removeItem("employee_name");
+	sessionStorage.removeItem("employee_rights");
+	location.replace("main.html");
 }
